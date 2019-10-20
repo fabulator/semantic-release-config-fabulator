838e7303edac7bf900d2cdd6a93bbc56ce810f81
Michal Ozogán <michal@ozogan.eu>
2019-10-20 18:23:04 +0200

    Added: Link to commits in messages.

--- src/generate-notes.js
+++ src/generate-notes.js
@@ -5,24 +5,31 @@ const {
 } = require('@socifi/commitlint-config/src/types');
 const processCommits = require('./process-commits.js');
 
-module.exports = (settings, { commits, nextRelease }) => {
+module.exports = ({ repositoryUrl }, { commits, nextRelease, ...rest }) => {
     const changes = {};
+
+    const repo = repositoryUrl.replace('git@', 'https://').replace('github.com:', 'github.com/').replace('.git', '/');
 
     // convert commit messages to universal format
     processCommits(commits).map((commit) => {
-        const [type, ...message] = commit.message.split(':');
-        if (message.length === 0) {
-            return {
-                type: 'Other',
-                message: commit.message,
-            };
-        }
+            const [type, ...message] = commit.message.split(':');
 
-        return {
-            type: type.trim(),
-            message: message.join(':').trim(),
-        };
-    })
+            const hashLink = `[${commit.commit.short}](${repo}commit/${commit.hash}) `;
+
+            if (message.length === 0) {
+                return {
+                    type: 'Other',
+                    message: hashLink + commit.message,
+                    hash: commit.hash,
+                };
+            }
+
+            return {
+                type: type.trim(),
+                message: hashLink + message.join(':').trim(),
+                hash: commit.hash,
+            };
+        })
         // remove all commits that should not be in changelog
         .filter(item => ![TEST, IN_PROGRESS].includes(item.type))
         .filter(item => !item.message.includes('Merge branch'))
